a0aa9e581a637646295e45732623c75a9b2ac7f6
Arthur Detomi <arthurdetomi@gmail.com>
2023-07-06 10:43:08 -0300

    ✔️ Exercicio fatorial aprovado!

---
+++ roteiro0/12.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define endl '\n';
+
+#define _ ios_base<:sync_with_stdio(false); \
+cin.tie(0); cout.tie(0);
+
+#define FOR(i, m, n) for (int i = m; i < n; i++)
+
+#define MAXN 21
+
+vector<long long> fat(MAXN, 1);
+
+void calc_fat() {
+    FOR(i, 1, MAXN) fat[i] = fat[i - 1] * i;
+}
+
+int main(void) {
+    calc_fat();
+    int m, n;
+    while (cin >> m >> n) {
+        cout << fat[m] + fat[n] << endl;
+    }
+    return 0;
+}
