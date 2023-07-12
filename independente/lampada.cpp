aa9f7d12111c73e5230373c774c0be1e9ab24efc
Arthur Detomi <arthurdetomi@gmail.com>
2023-07-12 13:38:06 -0300

    🔨 Identando código com clang

--- independente/lampada.cpp
+++ independente/lampada.cpp
@@ -4,22 +4,22 @@ using namespace std;
 
 #define endl '\n'
 
-int main(){
+int main() {
     int n;
-	cin >> n; 
-	
+    cin >> n;
+
     int value;
     bool l_a = false, l_b = false;
-	for (int i = 0; i < n; i++) {
-		cin >> value;
+    for (int i = 0; i < n; i++) {
+        cin >> value;
         if (value == 1) {
             l_a = !l_a;
         } else {
             l_a = !l_a;
             l_b = !l_b;
         }
-	}
-    
+    }
+
     cout << l_a << endl;
     cout << l_b << endl;
     return 0;
