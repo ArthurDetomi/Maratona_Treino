624e9b6899f936a1e5eb01c071dfb7b292bae56b
Arthur Detomi <arthurdetomi@gmail.com>
2023-08-01 14:01:13 -0300

    Removendo estrutura desnecessária

--- roteiro2/bsearch1_with_hash.cpp
+++ roteiro2/bsearch1_with_hash.cpp
@@ -6,14 +6,11 @@ int main(void) {
     long long n, q;
     scanf("%lld %lld", &n, &q);
 
-    vector<long long> V(n);
-
     unordered_map<long long, int> M;
 
     pair<long long, int> P;
     for (int i = 0; i < n; i++) {
-        scanf("%lld", &V[i]); 
-        P.first = V[i];
+        scanf("%lld", &P.first); 
         P.second = i;
         M.insert(P);
     }
