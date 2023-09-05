46132876c59b738166c9903c4c4e2368ce597235
Arthur Detomi <arthurdetomi@gmail.com>
2023-09-05 20:34:41 -0300

    📓 Template c++

---
+++ template/template.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define fastio                                                                 \
+    ios_base::sync_with_stdio(false);                                          \
+    cin.tie(0);                                                                \
+    cout.tie(0);
+
+#define endl '\n'
+
+#define pb push_back
+
+int main() {
+    fastio;
+    return 0;
+}
