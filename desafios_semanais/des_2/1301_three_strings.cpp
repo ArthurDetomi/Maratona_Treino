42f6e9885d0583f41f42767b91d684a9d2d56c91
Arthur Detomi <arthurdetomi@gmail.com>
2023-09-30 20:50:08 -0300

    Mais um exercicio resolvido desafio semanal

---
+++ desafios_semanais/des_2/1301_three_strings.cpp
@@ -0,0 +1,57 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define fastio                                                                 \
+    ios_base::sync_with_stdio(false);                                          \
+    cin.tie(0);                                                                \
+    cout.tie(0);
+
+#define endl '\n'
+
+#define pb push_back
+
+#define mp make_pair
+
+#define f first
+
+#define s second
+
+int t;
+
+
+bool eh_valido(char ai, char bi, char ci) {
+    return (ai == bi && ci == ai) || (ai != bi && (ci == ai || ci == bi));
+}
+
+int main() {
+    fastio;
+
+    cin >> t;
+    
+    string a, b, c;
+
+    while (t--) {
+        cin >> a;
+
+        cin >> b; 
+
+        cin >> c;
+
+        int tam = a.size();
+
+        bool eh_possivel = true;
+
+        for (int i = 0; i < tam; i++) {
+            if (!eh_valido(a[i], b[i], c[i])) {
+                eh_possivel = false;
+                break;
+            }
+        }
+
+        cout << (eh_possivel ? "YES\n":"NO\n");
+    }
+
+
+    return 0;
+}
