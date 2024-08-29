25195c225da5158572d30647274bae1c6c3bf2ef
Arthur Detomi <arthurdetomi@gmail.com>
2024-08-29 20:56:42 -0300

    Resolvendo exercicio com ajuda, mas resolvido

--- solo_exercicios_2024/smallest_range.cpp
+++ solo_exercicios_2024/smallest_range.cpp
@@ -1,42 +1,6 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-// Mas entendi a resolução da outra pessoa
-vector<int> smallestRange(vector<vector<int>> nums) {
-  vector<pair<int, int>> vec;
-  vector<int> res;
-  int k = nums.size();
-  for (int i = 0; i < k; i++) {
-    for (auto itr : nums[i]) {
-      vec.push_back({itr, i});
-    }
-  }
-  sort(vec.begin(), vec.end());
-  unordered_map<int, int> cnt;
-
-  int l = 0, r = 0;
-  while (r < vec.size()) {
-    cnt[vec[r].second]++;
-
-    while (cnt.size() == k) {
-      if (res.empty() || vec[r].first - vec[l].first < res[1] - res[0]) {
-        res = {vec[l].first, vec[r].first};
-      }
-      cnt[vec[l].second]--;
-
-      if (cnt[vec[l].second] == 0) {
-        cnt.erase(vec[l].second);
-      }
-
-      l++;
-    }
-
-    r++;
-  }
-  return res;
-}
-
-// Não consegui resolver
 vector<int> smallestRange(vector<vector<int>> nums) {
   vector<pair<int, int>> vec;
 
@@ -48,47 +12,28 @@ vector<int> smallestRange(vector<vector<int>> nums) {
 
   sort(vec.begin(), vec.end());
 
-  //  Percorro a lista que está junto e ordenado
-
-  int qtd_lists = nums.size();
   vector<int> res;
+  unordered_map<int, int> count_map;
 
-  for (int i = 0; i < (int)vec.size() && (int)vec.size() - i >= qtd_lists;
-       i++) {
-    unordered_set<int> S;
+  int left = 0, right = 0;
+  while (right < (int)vec.size()) {
+    count_map[vec[right].second]++;
 
-    int first_position = i;
-    int last_position = i + (qtd_lists - 1);
+    while (count_map.size() == nums.size()) {
+      if (res.empty() || vec[right].first - vec[left].first < res[1] - res[0]) {
+        res = { vec[left].first, vec[right].first };
+      }
 
-    int count = first_position;
-    for (int j = 0; j < qtd_lists; j++) {
-      S.insert(vec[count].second);
-      count++;
+      count_map[vec[left].second]--;
+
+      if (count_map[vec[left].second] == 0) {
+        count_map.erase(vec[left].second);
+      }
+
+      left++;
     }
 
-    if ((int)S.size() == qtd_lists) {
-      cout << vec[first_position].first << " " << vec[last_position].first
-           << "\n";
-      if (res.empty()) {
-        res = {vec[first_position].first, vec[last_position].first};
-        // cout << res[0] << " " << res[1] << '\n';
-        continue;
-      }
-
-      int current_small_range = abs(res[1] - res[0]);
-      int current_range =
-          abs(vec[last_position].first - vec[first_position].first);
-
-      if (current_range < current_small_range) {
-        res = {vec[first_position].first, vec[last_position].first};
-      }
-      /*
-      if (current_range == current_small_range &&
-          vec[last_position].first < res[0]) {
-        res = {vec[first_position].first, vec[last_position].first};
-      }
-      */
-    }
+    right++;
   }
 
   return res;
