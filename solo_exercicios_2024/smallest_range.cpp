1213ba1015d0bd00c82732a13d8c70b75106afd8
Arthur Detomi <arthurdetomi@gmail.com>
2024-08-29 20:23:40 -0300

    Novo exercício

--- solo_exercicios_2024/smallest_range.cpp
+++ solo_exercicios_2024/smallest_range.cpp
@@ -1,6 +1,42 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+// Mas entendi a resolução da outra pessoa
+vector<int> smallestRange(vector<vector<int>> nums) {
+  vector<pair<int, int>> vec;
+  vector<int> res;
+  int k = nums.size();
+  for (int i = 0; i < k; i++) {
+    for (auto itr : nums[i]) {
+      vec.push_back({itr, i});
+    }
+  }
+  sort(vec.begin(), vec.end());
+  unordered_map<int, int> cnt;
+
+  int l = 0, r = 0;
+  while (r < vec.size()) {
+    cnt[vec[r].second]++;
+
+    while (cnt.size() == k) {
+      if (res.empty() || vec[r].first - vec[l].first < res[1] - res[0]) {
+        res = {vec[l].first, vec[r].first};
+      }
+      cnt[vec[l].second]--;
+
+      if (cnt[vec[l].second] == 0) {
+        cnt.erase(vec[l].second);
+      }
+
+      l++;
+    }
+
+    r++;
+  }
+  return res;
+}
+
+// Não consegui resolver
 vector<int> smallestRange(vector<vector<int>> nums) {
   vector<pair<int, int>> vec;
 
@@ -16,8 +52,9 @@ vector<int> smallestRange(vector<vector<int>> nums) {
 
   int qtd_lists = nums.size();
   vector<int> res;
-  
-  for (int i = 0; i < (int)vec.size() && (int)vec.size() - i >= qtd_lists;i++) {
+
+  for (int i = 0; i < (int)vec.size() && (int)vec.size() - i >= qtd_lists;
+       i++) {
     unordered_set<int> S;
 
     int first_position = i;
@@ -30,20 +67,27 @@ vector<int> smallestRange(vector<vector<int>> nums) {
     }
 
     if ((int)S.size() == qtd_lists) {
-      cout << vec[first_position].first << " "<< vec[last_position].first << "\n";
+      cout << vec[first_position].first << " " << vec[last_position].first
+           << "\n";
       if (res.empty()) {
         res = {vec[first_position].first, vec[last_position].first};
-        //cout << res[0] << " " << res[1] << '\n';
+        // cout << res[0] << " " << res[1] << '\n';
         continue;
       }
 
-      int small_range = res[1] - res[0];
-      int current_small_range =
-          vec[last_position].first - vec[first_position].first;
+      int current_small_range = abs(res[1] - res[0]);
+      int current_range =
+          abs(vec[last_position].first - vec[first_position].first);
 
-      if (current_small_range < small_range) {
+      if (current_range < current_small_range) {
         res = {vec[first_position].first, vec[last_position].first};
       }
+      /*
+      if (current_range == current_small_range &&
+          vec[last_position].first < res[0]) {
+        res = {vec[first_position].first, vec[last_position].first};
+      }
+      */
     }
   }
 
