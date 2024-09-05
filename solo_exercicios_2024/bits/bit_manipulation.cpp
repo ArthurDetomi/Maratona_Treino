11ab32320d41783c7c1ea28d39f2946ea00d176d
Arthur Detomi <arthurdetomi@gmail.com>
2024-09-05 11:31:14 -0300

    Bit manipulation

--- solo_exercicios_2024/bits/bit_manipulation.cpp
+++ solo_exercicios_2024/bits/bit_manipulation.cpp
@@ -25,8 +25,33 @@ void updateIthBit(int &n, int i, int v) {
   n = (n | mask);
 }
 
+void clearLastIBits(int &n, int i) {
+  int mask = (~0) << i;
+  n = n & mask;
+}
+
+void clearBitsInRange(int &n, int i, int j) {
+  int a = (~0) << (j + 1);
+  int b = (1 << i) - 1;
+  int mask = a | b;
+  n = n & mask;
+}
+
+void replaceBits(int &n, int i, int j, int m) {
+  clearBitsInRange(n, i, j);
+  int mask = (m << i);
+  n = n | mask;
+}
+
+bool powerOfTwo(int n) {
+  if ((n & (n - 1)) == 0) {
+    return true;
+  }
+  return false;
+}
+
 int main() {
-  int n, i;
+  int n, i, j, m;
   /*
   cout << "Get ithBit\n";
   cin >> n >> i;
@@ -41,11 +66,36 @@ int main() {
   cout << n << endl;
 
   clearIthBit(n, i);
-  */
+
 
   int v;
   cout << "Update ith Bit:\n";
   cin >> n >> i >> v;
   updateIthBit(n, i, v);
+  */
+  /*
+  int j;
+  cin >> n >> i >> j;
+  // clearLastIBits(n, i);
+  clearBitsInRange(n, i, j);
+
+
+
+  n = 15;
+  i = 1;
+  j = 3;
+  m = 2;
+
+  replaceBits(n, i, j, m);
+
+  cin >> n;
   cout << n << endl;
+  */
+  cin >> n;
+
+  if (powerOfTwo(n)) {
+    cout << "Is Power of Two\n";
+  } else {
+    cout << "Not is power of two\n";
+  }
 }
