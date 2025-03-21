fe5bd73da7afa25fc2d6f1a43a4ab14c8ecd6670
Arthur Detomi <arthurdetomi@gmail.com>
2025-03-21 14:06:21 -0300

    chapter 1: Implementing book solution of palindrome permutation

--- cracking_coding_interview/chapter_1/palindrome_permutation.cpp
+++ cracking_coding_interview/chapter_1/palindrome_permutation.cpp
@@ -3,6 +3,7 @@
 
 using namespace std;
 
+// My solution
 bool is_palindrome_permutation(string str) {
   unordered_map<char, int> M;
 
@@ -25,12 +26,51 @@ bool is_palindrome_permutation(string str) {
   return count_odd <= 1;
 }
 
+vector<int> build_char_frequency(string phrase) {
+  vector<int> table('z' - 'a' + 1, 0);
+
+  for (char c : phrase) {
+    int x = c - 'a';
+
+    if (x != -1) {
+      table[x]++;
+    }
+  }
+
+  return table;
+}
+
+bool check_max_one_odd(vector<int> table) {
+  bool found_odd = false;
+
+  for (int count : table) {
+    if (count % 2 == 1) {
+      if (found_odd) {
+        return false;
+      }
+      found_odd = true;
+    }
+  }
+
+  return true;
+}
+
+// Book solution
+bool is_permutation_of_palidrome(string phrase) {
+  vector<int> table = build_char_frequency(phrase);
+  return check_max_one_odd(table);
+}
+
 int main() {
   string str;
 
   getline(cin, str);
 
   cout << (is_palindrome_permutation(str)
+               ? "Is possible palindrome permutation\n"
+               : "Not is possible palidrome permutation\n");
+
+  cout << (is_permutation_of_palidrome(str)
                ? "Is possible palindrome permutation\n"
                : "Not is possible palidrome permutation\n");
 
